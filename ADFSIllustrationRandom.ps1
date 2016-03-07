f43757982a5d84b149cd76c1a50ae41d650f26b6
unknown <kyles@JWS09.jaythom.local>
2016-03-07 17:22:26 +1100

    Initial Code Commit.

---
+++ ADFSIllustrationRandom.ps1
@@ -0,0 +1,3 @@
+﻿$files = dir -Path 'C:\ADFS Images\ADFS-*.png' -Recurse
+$sample = $files | Get-Random -Count 1
+Set-AdfsWebTheme -TargetName default -Illustration @{path=$sample.FullName} -Verbose
